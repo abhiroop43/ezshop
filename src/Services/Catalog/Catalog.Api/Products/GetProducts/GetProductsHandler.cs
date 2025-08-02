3c9e9bc6ada34f39f4d6729f2da6cf6764173c4f
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-08-03 02:23:37 +0400

    feat(api): :sparkles: implement GetProductsQuery and handler for retrieving product list
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,18 @@
 namespace Catalog.Api.Products.GetProducts;
 
-public class GetProductsHandler
+public record GetProductsQuery : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        if (products.Count == 0) logger.LogWarning("No products saved");
+
+        return new GetProductsResult(products);
+    }
 }
