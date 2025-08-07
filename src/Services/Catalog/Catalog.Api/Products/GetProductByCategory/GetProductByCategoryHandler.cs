e1e03942a0880ab5bf06399eccd47354dd03ba90
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-08-07 14:38:41 +0400

    feat(api): :sparkles: implement GetProductByCategoryEndpoint and handler for retrieving products by category

--- src/Services/Catalog/Catalog.Api/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,22 @@
 namespace Catalog.Api.Products.GetProductByCategory;
 
-public class GetProductByCategoryHandler
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler(
+    IDocumentSession session,
+    ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
-    
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query,
+        CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        if (products.Count == 0) logger.LogWarning("No products found with category {0}", query.Category);
+
+        return new GetProductByCategoryResult(products);
+    }
 }
