a0263f83e433ae79846e59f2d6eb4e2b39c2401a
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-08-11 23:35:44 +0400

    feat(shopping-cart): :sparkles: enhance ShoppingCart and ShoppingCartItem classes with user ID and item properties
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- src/Services/Basket/Basket.Api/Models/ShoppingCart.cs
+++ src/Services/Basket/Basket.Api/Models/ShoppingCart.cs
@@ -2,5 +2,16 @@ namespace Basket.Api.Models;
 
 public class ShoppingCart
 {
-    
+    public ShoppingCart(string userId)
+    {
+        UserId = userId;
+    }
+
+    public ShoppingCart()
+    {
+    }
+
+    public string UserId { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
 }
