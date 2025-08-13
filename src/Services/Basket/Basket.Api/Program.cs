b6c245a146fc0cd5e47ddcc4c4197462456873d9
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-08-13 12:38:42 +0400

    feat(services): :sparkles: configure MediatR with behaviors and add Carter to the application

--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,9 +1,20 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
 // add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
 // configure the HTTP Request pipeline
+app.MapCarter();
 
 await app.RunAsync();
