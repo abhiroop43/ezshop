131b17f58514fce1c7ade18b5b7d3e6301ebfe8a
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-08-18 21:51:13 +0400

    feat(basket): :sparkles: implement caching for basket operations using Redis; add CachedBasketRepository and update DI configuration
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- src/Services/Basket/Basket.Api/Data/CachedBasketRepository.cs
+++ src/Services/Basket/Basket.Api/Data/CachedBasketRepository.cs
@@ -1,6 +1,38 @@
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
 namespace Basket.Api.Data;
 
-public class CachedBasketRepository
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
 {
-    
+    public async Task<ShoppingCart> GetBasket(string userId, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(userId, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cachedBasket))
+        {
+            var deserializedBasket = JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+
+            if (deserializedBasket != null) return deserializedBasket;
+        }
+
+        var basket = await repository.GetBasket(userId, cancellationToken);
+        await cache.SetStringAsync(userId, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasket(basket, cancellationToken);
+        await cache.SetStringAsync(basket.UserId, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userId, CancellationToken cancellationToken = default)
+    {
+        var isSuccess = await repository.DeleteBasket(userId, cancellationToken);
+        if (!isSuccess) return false;
+        await cache.RemoveAsync(userId, cancellationToken);
+        return true;
+    }
 }
