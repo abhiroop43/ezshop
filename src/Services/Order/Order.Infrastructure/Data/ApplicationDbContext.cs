116cce829a6f6773c6271d81f9efa613311376a7
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-12-13 02:21:15 +0400

    feat(order): Added DbContext and entity configs
    Introduced ApplicationDbContext and entity type configurations for Customer, Product, Order, and OrderItem. Registered the DbContext with dependency injection and updated appsettings.json to include a database connection string. Added necessary EF Core packages to the project file.

---
+++ src/Services/Order/Order.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Order.Domain.Models;
+
+namespace Order.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Domain.Models.Order> Orders => Set<Domain.Models.Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+
+}
---
+++ src/Services/Order/Order.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,2 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Order.Infrastructure.Data;
