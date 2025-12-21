80dafe313061b4fa70d4ee5b7a640ed1d1525e42
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-12-21 23:23:54 +0400

    feat(order): Add pagination support and refactor order queries
    Introduced PaginatedResult and PaginationRequest for reusable pagination. Refactored GetOrdersQuery and handler to support paginated results. Simplified GetOrdersByCustomerQuery and its handler. Cleaned up code structure and improved .csproj formatting.

--- src/Services/Order/Order.Application/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
+++ src/Services/Order/Order.Application/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace Order.Application.Orders.Queries.GetOrders
+﻿using BuildingBlocks.Pagination;
+using Order.Application.Extensions;
+
+namespace Order.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersQueryHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
 {
-    internal class GetOrdersQueryHandler
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
     {
+        var totalOrders = await dbContext.Orders.CountAsync(cancellationToken);
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(query.PaginationRequest.PageIndex * query.PaginationRequest.PageSize)
+            .Take(query.PaginationRequest.PageSize)
+            .ToListAsync(cancellationToken);
+        var paginatedOrders = new PaginatedResult<OrderDto>(
+            query.PaginationRequest.PageIndex,
+            query.PaginationRequest.PageSize,
+            totalOrders,
+            orders.ToOrderDtoList()
+        );
+        return new GetOrdersResult(paginatedOrders);
     }
 }
