a722f058bcb1b23256f1eeb1db7ef2662cccb832
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-12-26 14:28:12 +0400

    fix(order): fixed the issue of update order returning success flag as false even though the order was updated successfully

--- src/Services/Order/Order.Api/Endpoints/UpdateOrder.cs
+++ src/Services/Order/Order.Api/Endpoints/UpdateOrder.cs
@@ -4,7 +4,7 @@ namespace Order.Api.Endpoints;
 
 public record UpdateOrderRequest(OrderDto Order);
 
-public record UpdateOrderResponse(bool Success);
+public record UpdateOrderResponse(bool IsSuccess);
 
 public class UpdateOrder : ICarterModule
 {
